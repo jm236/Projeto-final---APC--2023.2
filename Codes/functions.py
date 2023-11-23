c870db046eab2369fd045f1662c82c5182139bcb
João Marcelo Costa de Santana <joaomarcelocosta14@gmail.com>
2023-11-23 14:02:54 -0300

    Atualizações no menu inicial

--- Codes/functions.py
+++ Codes/functions.py
@@ -14,3 +14,28 @@ def config_fps(fps):
     """
     clock = pygame.time.Clock()
     clock.tick(fps)
+
+def menu(tela, tam, largura, altura):
+    nome_do_jogo = 'GTA VII'
+    vermelho = (255, 0, 0)
+    branco = (255, 255, 255)
+    preto = (0, 0, 0)
+    
+    escrever(nome_do_jogo, tela, "Monospace", vermelho, 225, 40, tam * 10)
+
+    string = "1- Jogar"
+    escrever(string, tela, "Monospace", branco, ((altura // 2) - 60), ((largura // 2) - 40), tam * 3)   
+
+    string = "2- Configurações" 
+    escrever(string, tela, "Monospace", branco, ((altura // 2) - 60), ((largura // 2) - 20), tam * 3)  
+
+    string = "3- Ranking" 
+    escrever(string, tela, "Monospace", branco, ((altura // 2) - 60), ((largura // 2)), tam * 3) 
+
+    string = "4- Instruções" 
+    escrever(string, tela, "Monospace", branco, ((altura // 2) - 60), ((largura // 2) + 20), tam * 3)
+
+    string = "5- Sair" 
+    escrever(string, tela, "Monospace", branco, ((altura // 2) - 60), ((largura // 2) + 40), tam * 3)  
+    
+    pygame.display.flip()
--- Codes/main.py
+++ Codes/main.py
@@ -7,6 +7,10 @@ aviso_um = 'Bem vindo! Pressione Enter para continuar'
 tam = 5 # tamanho da tela
 fps = 30 # fps do jogo
 enter = 13 # codigo do enter no pygame
+inicio = True # variavel que indica que o jogo ainda esta na tela inicial 
+menu_inicial = False # variavel que indica se o jogo esta no menu 
+largura = 72 * tam
+altura = 128 * tam
 
 # cores utilizadas 
 vermelho = (255, 0, 0)
@@ -18,11 +22,11 @@ pygame.init()
 # configuracao do fps
 config_fps(fps)
 
-screen = pygame.display.set_mode((1280, 720)) # tamanho da tela inicial
+screen = pygame.display.set_mode((altura, largura)) # tamanho da tela inicial
 
 # escrita da msg inicial ao abrir o jogo
-escrever(nome_do_jogo, screen, "Monospace", vermelho, 400, 270, tam * 20)
-escrever(aviso_um, screen, "Monospace", branco, 500, 500, tam * 2)
+escrever(nome_do_jogo, screen, "Monospace", vermelho, 200, 135, tam * 10)
+escrever(aviso_um, screen, "Monospace", branco, 180, 250, tam * 2)
 
 pygame.display.flip()
 
@@ -34,8 +38,14 @@ while running:
             running = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == 13:
+            if event.key == 13 and inicio == True:
+                inicio, menu_inicial = False, True # o jogo nao esta mais no inicio
                 screen.fill(preto)
-                menu() # função que apresentará o menu inicial do game
+                menu(screen, tam, largura, altura) # função que apresentará o menu inicial do game
                 pygame.display.flip()
+
+            elif event.key == pygame.K_5 and menu_inicial == True:
+                    running = False
+                
+
 
